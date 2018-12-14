808be02ba745437560457be2bc30a5a827754213
unfound <unfound@github.com>
2018-12-14 20:05:39 +0800

    剔除无参数和显示来源url

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+'use strict';
+
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const url = require('url');
 
+// const reqUrl = 'github.com';
 let msg = [];
-
+// 数组转成字符串
 function arrToStr (arr) {
   let str = ''
   arr.forEach(item => {
@@ -19,9 +23,13 @@ var router = new Router({
 
 router.use(bodyParser());
 router.all('/', ctx => {
+  console.log(url.parse(ctx.url, true));
   const body = ctx.request.body
   console.log(body);
-  msg.push(JSON.stringify(body));
+  if (body && JSON.stringify(body) != '{}') {
+    msg.push(JSON.stringify(body));
+  }
+  ctx.response.set('Content-Type', 'text/plain;charset=utf-8')
   ctx.body = arrToStr(msg);
 });
 
